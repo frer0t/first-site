3511100acf7d3bae2c85ec3c40ba819009680da8
frerot <149209283+frer0t@users.noreply.github.com>
2024-03-03 19:12:45 +0200

    finished blogs Responsiveness

--- blogspage.js
+++ blogspage.js
@@ -11,3 +11,25 @@ buttonBackTop.addEventListener('click', function (e) {
 
  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+const humburger = document.querySelector(".humburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const mobilenav = document.querySelector(".mobilenav");
+
+const mobileLinks = mobilenav.childNodes;
+
+mobileLinks.forEach(cur => cur.addEventListener('click', function () {
+ bar1.classList.toggle("animatebar1");
+ bar2.classList.toggle("animatebar2");
+ bar3.classList.toggle("animatebar3");
+ mobilenav.classList.toggle("opendrawer");
+}));
+
+humburger.addEventListener("click", function () {
+ bar1.classList.toggle("animatebar1");
+ bar2.classList.toggle("animatebar2");
+ bar3.classList.toggle("animatebar3");
+ mobilenav.classList.toggle("opendrawer");
+});
