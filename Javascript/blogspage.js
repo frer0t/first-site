fe21bb8fc8d733475fd17fcfbb77c332b3aacd13
ntwalifrerot <ntwalifrerotjacques@gmail.com>
2024-03-15 18:23:42 +0200

    Change some codes

--- Javascript/blogspage.js
+++ Javascript/blogspage.js
@@ -1,7 +1,11 @@
-const buttonBackTop = document.querySelector('.backtop');
 const blogs = document.querySelector('.blogs');
 const subscribe = document.querySelector('.subscribe');
 const opener = document.querySelector('.opener');
+const humburger = document.querySelector(".humburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const mobilenav = document.querySelector(".mobilenav");
 
 opener.addEventListener('click', () => {
  subscribe.classList.toggle('close');
@@ -12,24 +16,12 @@ window.onload = function () {
  blogs.style.transform = 'translateY(0)';
 };
 
-const humburger = document.querySelector(".humburger");
-const bar1 = document.querySelector(".bar1");
-const bar2 = document.querySelector(".bar2");
-const bar3 = document.querySelector(".bar3");
-const mobilenav = document.querySelector(".mobilenav");
-
-const mobileLinks = mobilenav.childNodes;
-
-mobileLinks.forEach(cur => cur.addEventListener('click', function () {
- bar1.classList.toggle("animatebar1");
- bar2.classList.toggle("animatebar2");
- bar3.classList.toggle("animatebar3");
- mobilenav.classList.toggle("opendrawer");
-}));
-
 humburger.addEventListener("click", function () {
  bar1.classList.toggle("animatebar1");
  bar2.classList.toggle("animatebar2");
  bar3.classList.toggle("animatebar3");
  mobilenav.classList.toggle("opendrawer");
 });
+
+
+
